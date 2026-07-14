2d686341125412983f4944d3f7a2965d0a4ffb4e
Simon Condon <simoncondon@hotmail.com>
2026-07-14 12:08:12 +0100

    oops, xml doc typo

--- src/SCDearImGui.MonoGame/IInputCaptureState.cs
+++ src/SCDearImGui.MonoGame/IInputCaptureState.cs
@@ -3,10 +3,10 @@
 /// <summary>
 /// <para>
 /// Interface for types that store indications of whether mouse or keyboard input has been captured
-/// by some component, and should not therefore be used by anything else.
+/// by some component, and should therefore not be used by anything else.
 /// </para>
 /// <para>
-/// An implementation of this type can optioanlly be passed to <see cref="ImGuiRenderer"/>, to keep it
+/// An implementation of this type can optionally be passed to <see cref="ImGuiRenderer"/>, to keep it
 /// from consuming input when appropriate, and keep track of when it has captured input.
 /// </para>
 /// <para>
